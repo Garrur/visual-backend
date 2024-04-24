ef5c3f6e3f904f1254d39d88a36bb41fc63bd908
Utkarsh Raj <uraj33175@gmail.com>
2024-04-24 10:08:34 +0530

    inital

---
+++ src/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const apiRoutes = require('./routes/api');
+require('dotenv').config(); 
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connection
+const MONGO = process.env.MONGODB_URI
+mongoose.connect((MONGO), {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', (err) => {
+  console.error('MongoDB connection error:', err);
+});
+
+db.once('open', () => {
+  console.log('MongoDB connected successfully');
+});
+
+// Routes
+app.use('/api', apiRoutes);
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
